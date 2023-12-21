2f8c785e7520c46b62ee39595b6fbe6fef37a1fd
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2023-12-21 04:18:26 +0200

    feat: fix sql query.

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -8,6 +8,7 @@ CREATE DATABASE IF NOT EXISTS `hbnb_test_db`;
 SET GLOBAL validate_password.policy=0;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
 USE performance_schema;
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
