a37f93e27024e6b59af4bc17096c51045fdf2d92
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2023-12-21 02:11:26 +0200

    refactor: amenity class.

--- models/amenity.py
+++ models/amenity.py
@@ -10,6 +10,6 @@ class Amenity(BaseModel, Base):
     """
     Amenity class represents a feature or service provided by a property.
     """
-    _tablename_ = "amenities"
+    __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
     place_amenities = relationship("Place", secondary=place_amenity)
