d1c3668c0a9674213749353ab71833f996348995
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2023-12-21 02:12:38 +0200

    refactor: review class.

--- models/review.py
+++ models/review.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
 import os
-from models.base_model import BaseModel, Base
+from models.base_model import BaseModel, Base   
 from sqlalchemy import Column, Integer, String, ForeignKey
 
 
 class Review(BaseModel, Base):
-    """Review class stores review information.
-
-    Attributes:
-        place_id (str): The ID of the place associated with the review.
-        user_id (str): The ID of the user who wrote the review.
-        text (str): The content of the review.
-    """
+    """ Review classto store review information """
+    # place_id = ""
+    # user_id = ""
+    # text = ""
 
     __tablename__ = 'reviews'
     place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
